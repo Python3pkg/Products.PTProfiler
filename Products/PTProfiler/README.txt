009a8a82e40e3e382af3c2d5bf1b637112704fcb
Sylvain Viollon <thefunny@gmail.com>
2013-12-16 11:12:44 +0100

    Recreate Products sekeleton and update project information.

--- Products/PTProfiler/README.txt
+++ Products/PTProfiler/README.txt
@@ -1,5 +1,6 @@
+==========
 PTProfiler
-**********
+==========
 
 PTProfiler is a small profiling system for page templates in Zope
 2. It times each TAL expression and lists the results in a table
@@ -20,3 +21,9 @@ id). When you click one of the items, you will see a list of all the
 expression calls in the template, ordered by total time spent on that
 expression.
 
+
+Code repository
+===============
+
+You can find the code of this extension in Git:
+https://github.com/infrae/Products.PTProfiler
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.4.1dev'
+
+setup(name='Products.PTProfiler',
+      version=version,
+      description="PageTemplate profiler for Zope 2",
+      long_description=open(os.path.join("Products", "PTProfiler", "README.txt")).read() + "\n" +
+                       open(os.path.join("Products", "PTProfiler", "HISTORY.txt")).read(),
+      classifiers=[
+          "Framework :: Zope2",
+          "License :: OSI Approved :: BSD License",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords='zope2 page template profiler',
+      author='Guido Wesdorp and Infrae',
+      author_email='info@infrae.com',
+      url='https://github.com/infrae/Products.PTProfiler',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          ],
+      )
